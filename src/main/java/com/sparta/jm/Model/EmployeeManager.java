c8e1cb88fb16b97f860b8329dcb5c222d5f33d82
James Mac <jamesmac9879@gmail.com>
2019-10-31 10:30:06 +0000

    Initial commit

--- src/main/java/com/sparta/jm/Model/EmployeeManager.java
+++ src/main/java/com/sparta/jm/Model/EmployeeManager.java
@@ -1,17 +1,10 @@
 package com.sparta.jm.Model;
 
-import com.sparta.jm.Controller.DAO;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-
 public class EmployeeManager {
 
     public void readEmployeeCSV(){
         CSVReader csvReader = new CSVReader();
-        csvReader.csvReader("resources/EmployeeRecords.csv");
+        csvReader.csgivReader("resources/EmployeeRecords.csv");
 
         csvReader.passToDAO();
     }
